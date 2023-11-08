95e23edb62e120d13054df1f816f79cac3398f8a
Powlinett <pauline.eustachy@gmail.com>
2023-11-08 11:24:34 +0100

    add lecture examples

--- templating/lecture/javascript/application.js
+++ templating/lecture/javascript/application.js
@@ -1,10 +1,52 @@
 import Mustache from 'mustachejs'
-// import { createApp } from 'vue'
+import { createApp } from 'vue'
 
 const url = "http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7";
 
 const results = document.querySelector('#results');
 const template = document.querySelector('#movieCardTemplate');
+
+// Example with vanilla javascript
+
+// fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
+//   .then(response => response.json())
+//   .then((data) => {
+//     data.Search.forEach((result) => {
+//       const movieCard = `
+//         <div class='col-3'>
+//           <div class="card">
+//             <img src="${result.Poster}" class="card-img-top" alt="${result.Title} poster">
+//             <div class="card-body">
+//               <h6 class="card-title">${result.Title}</h6>
+//               <p class="card-text">${result.Year}</p>
+//               <a href="https://www.imdb.com/title/${result.imdbID}" class="btn btn-primary" target="_blank">Go on IMDB</a>
+//             </div>
+//           </div>
+//         </div>
+//       `;
+
+//     results.insertAdjacentHTML('beforeend', movieCard);
+//     });
+//   });
+
+
+// Example with <template> HTML tag
+
+// fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
+// .then(response => response.json())
+// .then((data) => {
+//     data.Search.forEach((movie) => {
+//       const clone = template.content.cloneNode(true);
+//       clone.querySelector("img").src = movie.Poster
+//       clone.querySelector("h6").textContent = movie.Title
+//       clone.querySelector("p").textContent = movie.Year
+//       clone.querySelector("a").href = `https://www.imdb.com/title/${movie.imdbID}`
+//       results.appendChild(clone);
+//     });
+//   });
+
+
+// Example with Mustache templating library
 
 // GET data from omDB API
 fetch(url)
